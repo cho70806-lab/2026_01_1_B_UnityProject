5d3879a18d0da80e2985aa200c27bbd768770ce2
cho70806 <cho70806@gmail.com>
2026-03-31 11:49:30 +0900

    [fead]Prefabs 생성

---
+++ Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,52 @@
+using System.Xml.Serialization;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxLives = 3;
+    public int curentLives;
+
+    public float invincibleTime = 1.0f;
+    public bool isinvincble = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        curentLives = maxLives;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("missile"))
+        {
+            curentLives--;
+            Destroy(other.gameObject);
+
+            if(curentLives <- 0)
+            {
+                GameOver();
+            }
+        }
+    }
+
+    void GameOver()
+    {
+        gameObject.SetActive(false);
+        Invoke("RestartGame", 3.0f);
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
